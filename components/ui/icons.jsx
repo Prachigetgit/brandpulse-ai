e744bc7e1977a9e2897aac1ce43d06a2965b6c37
Prachigetgit <126250683+Prachigetgit@users.noreply.github.com>
2025-04-24 14:10:13 +0530

    auth and dashboard setup using clerk

---
+++ components/ui/icons.jsx
@@ -0,0 +1,7 @@
+import { FaGithub, FaGoogle, FaSpinner } from "react-icons/fa";
+
+export const Icons = {
+  gitHub: (props) => <FaGithub {...props} />,
+  google: (props) => <FaGoogle {...props} />,
+  spinner: (props) => <FaSpinner {...props} />,
+};
